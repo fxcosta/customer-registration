972d099905e57c4ee3d12dbd376ebbb13c49cc84
Candido Souza <candido_souzza@hotmail.com>
2015-09-29 08:34:07 -0300

    Configurando modulo

--- public/customers/customers.js
+++ public/customers/customers.js
@@ -1,4 +1,14 @@
 (function() {
+    'use strict';
+
     angular.module('customers', ['ngRoute', 'ngResource'])
+
+        .config(['$routeProvider', function ($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'customers/views/index.html',
+                    controller: 'customersCtrl'
+                })
+        }]);
 
 })();
