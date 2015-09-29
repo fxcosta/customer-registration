4c862b10d3db702906f1b3eb02206558e077172a
Candido Souza <candido_souzza@hotmail.com>
2015-09-29 18:23:37 -0300

    Configurando e rodando o Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -11,12 +11,14 @@ elixir(function(mix) {
     ], 'public/lib/css/main.css');
 
     mix.scripts([
-        '/../../../node_modules/jquery/dist/jquery.mim.js',
+        '/../../../node_modules/jquery/dist/jquery.min.js',
         '/../../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
         '/../../../node_modules/angular/angular.min.js',
         '/../../../node_modules/angular-route/angular-route.min.js',
         '/../../../node_modules/angular-resource/angular-resource.min.js',
         '/../../../public/customers/customers.js',
+        '/../../../public/customers/controllers/customersCtrl.js',
+        '/../../../public/customers/services/customersService.js',
         'main.js'
     ], 'public/lib/js/main.min.js');
 
